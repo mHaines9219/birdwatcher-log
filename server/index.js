1f535d1b9d91138e30dc519628a33f9bf905fe81
mHaines331 <mhaines9219@gmail.com>
2024-02-15 21:18:18 -0800

    refactor router

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express, { json } from 'express'; // ES6 import syntax
 import { PORT, mongoDBURL } from './config/config.js';
 import mongoose from 'mongoose';
-import Bird from './models/Bird.js';
+import birds from './routes/birds.js';
 
 const app = express();
 
@@ -12,66 +12,12 @@ app.get('/', (req, res) => {
   return res.status(234).send('Hello from the server!');
 });
 
+app.use('/birds', birds);
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-app.post('/birds', async (req, res) => {
-  try {
-    if (!req.body.name) {
-      return res.status(400).send('All fields are required');
-    }
-    const newBird = {
-      name: req.body.name,
-      scientificName: req.body.scientificName,
-      habitat: req.body.habitat,
-      lifeExpectancy: req.body.lifeExpectancy,
-    };
-    const bird = await Bird.create(newBird);
-    console.log(bird);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.get('/birds', async (req, res) => {
-  try {
-    const birds = await Bird.find({});
-    return res.status(200).json({ birds });
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.get('/birds/:id', async (req, res) => {
-  try {
-    const birdById = await Bird.findById(req.params.id);
-    return res.status(200).json({ birdById });
-  } catch (err) {
-    console.log(err);
-  }
-});
-app.put('/birds/:id', async (req, res) => {
-  try {
-    if (!req.body.name) {
-      return res.status(400).send('All fields are required');
-    }
-    const { id } = req.params;
-    const result = await Bird.findByIdAndUpdate(id, req.body);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.delete('/birds/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const result = await Bird.findByIdAndDelete(id);
-    return res.status(200).send('Bird deleted');
-  } catch (err) {
-    console.log(err);
-  }
-});
 mongoose
   .connect(mongoDBURL)
   .then(() => {
---
+++ server/routes/birds.js
@@ -0,0 +1,62 @@
+import express from 'express';
+const router = express.Router();
+import Bird from '../models/Bird.js';
+
+router.post('/', async (req, res) => {
+  try {
+    if (!req.body.name) {
+      return res.status(400).send('All fields are required');
+    }
+    const newBird = {
+      name: req.body.name,
+      scientificName: req.body.scientificName,
+      habitat: req.body.habitat,
+      lifeExpectancy: req.body.lifeExpectancy,
+    };
+    const bird = await Bird.create(newBird);
+    console.log(bird);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const birds = await Bird.find({});
+    return res.status(200).json({ birds });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const birdById = await Bird.findById(req.params.id);
+    return res.status(200).json({ birdById });
+  } catch (err) {
+    console.log(err);
+  }
+});
+router.put('/:id', async (req, res) => {
+  try {
+    if (!req.body.name) {
+      return res.status(400).send('All fields are required');
+    }
+    const { id } = req.params;
+    const result = await Bird.findByIdAndUpdate(id, req.body);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Bird.findByIdAndDelete(id);
+    return res.status(200).send('Bird deleted');
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+export default router;
