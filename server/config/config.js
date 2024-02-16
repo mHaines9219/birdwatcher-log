cec7523b9d32d9c8f256a920ac97e1dd6cfc0620
mHaines331 <mhaines9219@gmail.com>
2024-02-15 16:45:31 -0800

    scaffolding backend

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5001;
+
+export const mongoDBURL =
+  'mongodb+srv://sudo:password1234@cluster0.tzwi8fs.mongodb.net/?retryWrites=true&w=majority';
--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 import express from 'express'; // ES6 import syntax
-import { PORT } from './config/config.js';
+import { PORT, mongoDBURL } from './config/config.js';
+import mongoose from 'mongoose';
 
 const app = express();
+
+app.get('/', (req, res) => {
+  console.log(req.headers);
+  return res.status(234).send('Hello from the server!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log('Error:', err);
+  });
