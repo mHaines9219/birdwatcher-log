e74953e12c794ba49b1d30f1544f92b15b8dbc02
mHaines331 <mhaines9219@gmail.com>
2024-02-15 17:13:11 -0800

    CR funx implement

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5001;
 
 export const mongoDBURL =
-  'mongodb+srv://sudo:password1234@cluster0.tzwi8fs.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://sudo:password1234@cluster0.tzwi8fs.mongodb.net/birdwatcher?retryWrites=true&w=majority';
