7da8acaf7d7181fa8aca27c181208089cd490242
mHaines331 <mhaines9219@gmail.com>
2024-02-19 09:51:45 -0800

    link client and server

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Spinner from '../../components/Spinner/Spinner';
+import { Link } from 'react-router-dom';
 export default function Home() {
-  return <div>Home Page</div>;
+  const [birds, setBirds] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('http://localhost:5001/birds')
+      .then((res) => {
+        setBirds(res.data.birds);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      {birds.map((bird, index) => (
+        <div key={index}>
+          <Link to={`/bird/${bird._id}`}>{bird.name}</Link>
+        </div>
+      ))}
+    </>
+  );
 }
