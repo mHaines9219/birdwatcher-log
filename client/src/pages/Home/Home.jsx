72ef1414e37757b999c271dc52d26f69455119f2
mHaines331 <mhaines9219@gmail.com>
2024-02-22 00:14:59 -0800

    styling swiper

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -33,36 +33,37 @@ export default function Home({ setBirds, birds }) {
     <>
       <h1 className="welcome-header">Bird Journal</h1>
       <br></br>
-
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        className="mySwiper"
-        freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination]}
-        style={{
-          '--swiper-pagination-color': '#FFBA08',
-          '--swiper-pagination-bullet-inactive-color': '#999999',
-          '--swiper-pagination-bullet-inactive-opacity': '1',
-          '--swiper-pagination-bullet-size': '16px',
-          '--swiper-pagination-bullet-horizontal-gap': '6px',
-        }}
-      >
-        {birds.map((bird, index) => (
-          <SwiperSlide className="swiper-card" key={index}>
-            <Link className="card-link" to={`/birds/details/${bird._id}`}>
-              <h3 className="bird-name-header">{bird.name}</h3>
-              <h5 className="bird-sci-name">{bird.scientificName}</h5>
-              <p className="bird-date">
-                {new Date(bird.createdAt).toLocaleDateString('en-US')}
-              </p>
-            </Link>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="swiper-wrapper">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          className="mySwiper"
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          style={{
+            '--swiper-pagination-color': '#FFBA08',
+            '--swiper-pagination-bullet-inactive-color': '#999999',
+            '--swiper-pagination-bullet-inactive-opacity': '1',
+            '--swiper-pagination-bullet-size': '16px',
+            '--swiper-pagination-bullet-horizontal-gap': '6px',
+          }}
+        >
+          {birds.map((bird, index) => (
+            <SwiperSlide className="swiper-card" key={index}>
+              <Link className="card-link" to={`/birds/details/${bird._id}`}>
+                <h3 className="bird-name-header">{bird.name}</h3>
+                <h5 className="bird-sci-name">{bird.scientificName}</h5>
+                <p className="bird-date">
+                  {new Date(bird.createdAt).toLocaleDateString('en-US')}
+                </p>
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
 
       <div className="log-btn">
         <Link to={'/birds/create'}>Log a Bird Sighting</Link>
